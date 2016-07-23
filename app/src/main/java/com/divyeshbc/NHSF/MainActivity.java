356fd6749b3aea3cdc4c4f02a8bed0ea380426d7
Mickey <nagarm@uni.coventry.ac.uk>
2016-07-23 01:51:49 +0100

    Add files via upload

--- app/src/main/java/com/divyeshbc/NHSF/MainActivity.java
+++ app/src/main/java/com/divyeshbc/NHSF/MainActivity.java
@@ -1,18 +1,15 @@
 package com.divyeshbc.NHSF;
 
-import android.content.pm.ActivityInfo;
-import android.os.Build;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.NotificationCompat;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.WindowManager;
 
-import com.divyeshbc.NHSF.tabs.SlidingTabLayout;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 import com.divyeshbc.NHSF.tabs.aboutUs.Tab1;
 import com.divyeshbc.NHSF.tabs.campus.Tab2;
 import com.divyeshbc.NHSF.tabs.events.Tab3;
@@ -20,170 +17,92 @@ import com.divyeshbc.NHSF.tabs.learning.Tab4;
 import com.divyeshbc.NHSF.tabs.sewa.Tab5;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
 
-    private ViewPager mPager;
-    private SlidingTabLayout mTabs;
-
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    FragmentTransaction fragmentTransaction;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        //Setting the orientation to Portrait Only
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        actionBarDrawerToggle= new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
-        // To track statistics around application
-        //ParseAnalytics.trackAppOpened(getIntent());
-
-        // inform the Parse Cloud that it is ready for notifications
-        //ParseInstallation.getCurrentInstallation().saveInBackground();
-
-        //Creating a new object of notification builder
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
-
-        notificationBuilder.setSmallIcon(getNotificationIcon(notificationBuilder));
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            //Set Status bar colour to Orange
-            getWindow().setStatusBarColor(getResources().getColor(R.color.baseSecondaryBackgroundColour));
-
-        }
-        /*
-        else {
-            getWindow().setStatusBarColor(getResources().getColor(R.color.md_black_1000));
-        }
-        */
-
-        //Calling Activate Toolbar method
-        activateToolBar();
-
-        mPager = (ViewPager) findViewById(R.id.pager);
-        //Setting the Adapter on the view pager first. Passing the fragment manager through as an argument
-        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
-        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
-
-        //Setting the custom Tab View as the Sliding Tabs Layout
-        mTabs.setCustomTabView(R.layout.custom_tab_view, R.id.tabText);
-
-        mTabs.setDistributeEvenly(true);
-
-        //mTabs.setSelectedIndicatorColors(getResources().getColor(R.color.tabIndicatorColour));
-
-        mTabs.setBackgroundColor(getResources().getColor(R.color.basePrimaryBackgroundColour));
-
-        //Setting the ViewPager as the tabs
-        mTabs.setViewPager(mPager);
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar enough_food it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        //int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        //enough_food (id == R.id.action_settings) {
-        //    return true;
-        //}
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    class MyPagerAdapter extends FragmentPagerAdapter {
-
-
-        String[] tabs;
-
-        public MyPagerAdapter(FragmentManager fm) {
-            super(fm);
-
-            //Initialising the strings array of the tabs
-            tabs = getResources().getStringArray(R.array.tabs);
-
-        }
-
-        /*
-        //Setting up integer array of icons
-        int icons[] = {R.drawable.about_us, R.drawable.campus, R.drawable.events, R.drawable.learning, R.drawable.sewa_campaigns};
-
-        //Defined from strings.xml
-        String[] tabText = getResources().getStringArray(R.array.tabs);
-
-        public MyPagerAdapter(FragmentManager fm) {
-            super(fm);
-            //Initialising the strings array of tabs
-            tabText = getResources().getStringArray(R.array.tabs);
-
-        }
-        */
-
-        @Override
-        public Fragment getItem(int position) {
-
-            if (position == 0) // Based on the position is we are returning the relevant tab
-            {
-                return new Tab1();
-
-            } else if (position == 1) {
-                return new Tab2();
-
-            } else if (position == 2) {
-                return new Tab3();
-
-            } else if (position == 3) {
-                return new Tab4();
-
-            } else {
-                return new Tab5();
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container,new NHSF_UK());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle("NHSF (UK)");
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.home:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new NHSF_UK());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Home");
+                        item.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_tab1:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new Tab1());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("About Us");
+                        item.setChecked(true);
+                        drawerLayout.closeDrawers();
+                      break;
+                    case R.id.nav_tab2:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new Tab2());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Campus");
+                        item.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_tab3:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new Tab3());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Events");
+                        item.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_tab4:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new Tab4());
+                        fragmentTransaction.commit();
+                        getSupportActionBar().setTitle("Learning");
+                        item.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_tab5:
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container,new Tab5());
+                        fragmentTransaction.commit();
+                        drawerLayout.closeDrawers();
+                        getSupportActionBar().setTitle("Sewa");
+                        item.setChecked(true);
+                        break;
+                }
+                return false;
             }
-        }
-
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-
-            //Return the text of the position clicked and display this as the title for the tab
-            return tabs[position];
-        }
-
-        @Override
-        public int getCount() {
-            return 5;
-        }
+        });
     }
 
-
-    //This method will determine the user's operating system and based on this will set the app notification icon accordingly
-    private int getNotificationIcon(NotificationCompat.Builder notificationBuilder) {
-
-        //If the OS version is greater than or equal to lollipop
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-
-            int color = 0xFFD66F;
-            //Set the background colour of the app icon to orange
-            notificationBuilder.setColor(color);
-            //Use the transparent icon (As per Google Design Guidelines for Icons for Lollipop and above)
-            return R.mipmap.nhsf_twentyfifth_app_icon_transparent;
-
-        } else {
-            //Else use the standard colour icon
-            return R.mipmap.nhsf_25th_app_logo_androidx144;
-        }
-
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+      super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
     }
 }
 
